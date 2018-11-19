c7dd2a1e6db21a52628c1a3d158aa7fd98e2d612
Hai <hai@localhost.localdomain>
2018-11-19 01:41:31 -0600

    still working on viterbi

--- hidden-markov.py
+++ hidden-markov.py
@@ -23,7 +23,32 @@ for i in range(1000):
 rolls = np.array(rolls)
 die = np.array(die)
 
-k,m = np.unique(rolls, return_counts = True)
-print(np.asarray((k,m)).T)
+# k,m = np.unique(rolls, return_counts = True)
+# print(np.asarray((k,m)).T)
+# z,l = np.unique(die, return_counts=True)
 
-z,l = np.unique(die, return_counts=True)
+phi_1 = -np.log(0.5) - np.log(modelTable[0,rolls[1]-1])
+
+def phi_t(z,x):
+    return -np.log(modelTable[z,x])
+
+def psi_t(z, z_prev):
+    return -np.log(A[z_prev, z])
+
+K = 2
+T = rolls.shape[0]
+T1 = np.empty((K,T))
+T2 = np.empty((K,T))
+
+T1[:, 0] = -np.log(modelTable[0, rolls[i]-1])
+T2[:, 0] = 0
+
+for i in range(1, T):
+    # T1[:,i] = np.min(T1[:,i-1] -np.log(A) - np.log(modelTable[np.newaxis, :, rolls[i]-1]), 1)
+    # T2[:,i] = np.argmin(T1[:,i-1] -np.log(A) - np.log(modelTable[np.newaxis, :, rolls[i]-1]), 1)
+    T1[:,i] = np.min(T1[:,i-1] -np.log(A) - np.log(modelTable[:, rolls[i]-1]), 1)
+    T2[:,i] = np.argmin(T1[:,i-1] -np.log(A) - np.log(modelTable[:, rolls[i]-1]), 1)
+
+z_t = np.array([np.argmin(T1[:,i]) for i in range(T)])
+
+print("Phew")
