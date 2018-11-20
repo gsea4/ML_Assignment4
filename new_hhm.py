84c5c8392ff388c40ea538b9015cbbcc3bbf65c7
HaiH <hai.huynh@okstate.edu>
2018-11-20 13:44:56 -0600

    deleted unncessary files

--- new_hhm.py
+++ new_hhm.py
@@ -1,11 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.misc import logsumexp
-# def logSumExp(ns):
-#     max = np.max(ns)
-#     ds = ns - max
-#     sumOfExp = np.exp(ds).sum()
-#     return max + np.log(sumOfExp)
+
 X = np.array([1,2,3,4,5,6])
 states = np.array([0,1])
 
@@ -15,7 +11,7 @@ A = np.array([[0.95, 0.05],[0.1, 0.9]])
 rolls = []
 die = []
 current_state = 0
-for i in range(100):
+for i in range(N):
     face = np.random.choice(X, p=B[current_state])
     rolls.append(face)
     die.append(current_state)
@@ -24,7 +20,6 @@ for i in range(100):
     if r > A[current_state][current_state]:
         current_state ^= 1
     
-
 rolls = np.array(rolls) - 1
 die = np.array(die)
 
@@ -32,9 +27,9 @@ K = 2
 T = rolls.shape[0]
 Alpha = np.empty((K,T))
 
+# Forward
 Alpha[:, 0] = np.log(0.5) + np.log(B[:,rolls[0]])
 # Alpha[:, 0] = 0.5 * B[:,rolls[0]]
-
 for t in range(1,T):
     for k in range(K):
         Alpha[k,t] = np.log(B[k, rolls[t]]) + logsumexp([Alpha[i,t-1] + np.log(A[i, k]) for i in range(K)])
@@ -42,9 +37,14 @@ for t in range(1,T):
 
 normalized_alpha = Alpha[1,:]/np.sum(Alpha, axis=0)
 
-# normalized_alpha = np.array([Alpha[:,i]/np.sum(Alpha[:,i]) for i in range(Alpha.shape[1])])
-# n_alpha = Alpha[0,:]/np.sum(Alpha[0])
-x = np.arange(0,100)
+# Backward
+Beta = np.empty((K,T))
+Beta[:, T-1] = 1
+for t in range(T-2, 0, -1):
+    for k in range(K):
+        Beta[k,t] = logsumexp([Beta[i,t+1] + np.log(B[k, rolls[t+1]]) + np.log(A[i,k]) for i in range(K)])
+
+x = np.arange(0,N)
 
 plt.plot(x, normalized_alpha)
 plt.plot(x, die)
