3eebee0f09e4a9a6bffa49792b4bf9f08a12ca5e
Hai H <haihuynhkbn@gmail.com>
2018-11-20 13:41:52 -0600

    f

--- hidden-markov.py
+++ hidden-markov.py
@@ -10,7 +10,7 @@ A = np.array([[0.95, 0.05],[0.1, 0.9]])
 rolls = []
 die = []
 current_state = 0
-for i in range(1000):
+for i in range(100):
     face = np.random.choice(X, p=modelTable[current_state])
     rolls.append(face)
     die.append(current_state)
@@ -29,6 +29,7 @@ T1 = np.empty((K,T))
 T2 = np.empty((K,T))
 
 Alpha[:, 0] = np.log(modelTable[0, rolls[0]-1]) + np.log(0.5)
+# Alpha[:, 0] = modelTable[:, rolls[0]-1] * 0.5
 T2[:, 0] = 0
 Z1 = np.zeros(T)
 
@@ -37,8 +38,9 @@ for i in range(1, T):
     for k in range(K):
         # T1[k,i] = np.max([T1[q,i-1] + np.log(A[k,q]) + np.log(modelTable[k, rolls[i]-1]) for q in range(K)])
         # T2[k,i] = np.argmax([T1[q,i-1] + np.log(A[k,q]) + np.log(modelTable[k, rolls[i]-1]) for q in range(K)])
-        Alpha[k,i] = np.log(modelTable[k, rolls[i]-1]) + np.sum([Alpha[q,i-1] + np.log(A[k,q]) for q in range(K)])
-        # Alpha[k,i] = modelTable[k,rolls[i]-1]*np.sum([Alpha[q,i-1] * A[k,q] for q in range(K)])
+        Alpha[k,i] = np.log(modelTable[k, rolls[i]-1]) + np.sum(np.log([Alpha[q,i-1] * A[q,k] for q in range(K)]))
+        # Alpha[k,i] = modelTable[k,rolls[i]-1]*np.sum([Alpha[q,i-1] * A[q,k] for q in range(K)])
+        pass
 
 # z_t = np.array([np.argmax(Alpha[range(K),i]) for i in range(T)])
 # normalized = T1[0,:]/np.sum(T1, axis=0)
